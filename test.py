511beba0492abbdccb66581c3364dac2738ed1be
Kythias <kythias@hotmail.co.uk>
2017-07-22 23:05:37 +0100

    Dictionary Check

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print "Hello World!"
+import enchant
+
+correct_word = "Hello World!"
+incorrect_word = "Hallo World!"
+d = enchant.Dict("en_GB")
+
+if d.check(correct_word) == True:
+    print "correct_word"
+
+if d.check(incorrect_word) == False:
+    print "All has gone according to plan"
